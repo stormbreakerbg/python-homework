be7cde4b5b84b08f31811be1b652906c25568b79
Georgy Angelov <georgyangelov@gmail.com>
2013-03-27 00:38:25 +0200

    Some extra tests for the third homework

--- 2013/03/sample_test.py
+++ 2013/03/sample_test.py
@@ -7,16 +7,25 @@ class PersonTest(unittest.TestCase):
     def setUp(self):
         self.adam = solution.Person(name='Adam', gender='M', birth_year=0)
         self.eva = solution.Person(name='Eva', gender='F', birth_year=0)
-        self.first_son = solution.Person(name='Kain', gender='M', birth_year=20,
-                            father=self.adam, mother=self.eva)
-        self.first_daughter = solution.Person(name='Pepa', gender='F', birth_year=22,
-                            father=self.adam, mother=self.eva)
+        self.first_son = solution.Person(name='Kain', gender='M',
+                                         birth_year=20, father=self.adam,
+                                         mother=self.eva)
+        self.first_daughter = solution.Person(name='Pepa', gender='F',
+                                              birth_year=22, father=self.adam,
+                                              mother=self.eva)
+        self.evas_secret_son = solution.Person(name='Ali Raza', gender='M',
+                                               birth_year=25, mother=self.eva)
+        self.third_daughter = solution.Person(name='Ferhunde', gender='F',
+                                              birth_year=45,
+                                              father=self.evas_secret_son)
 
     def tearDown(self):
         del self.adam
         del self.eva
         del self.first_son
         del self.first_daughter
+        del self.evas_secret_son
+        del self.third_daughter
 
     def test_attributes(self):
         self.assertIn('name', dir(self.adam))
@@ -26,6 +35,34 @@ class PersonTest(unittest.TestCase):
     def test_has_sister(self):
         self.assertEqual(self.first_son.get_sisters(), [self.first_daughter])
 
+    def test_has_brother(self):
+        self.assertEqual(self.first_son.get_brothers(), [self.evas_secret_son])
+
+    def test_common_mother_sisters(self):
+        self.assertEqual(self.evas_secret_son.get_sisters(), [
+            self.first_daughter
+        ])
+
+    def test_common_mother_brothers(self):
+        self.assertEqual(self.evas_secret_son.get_brothers(), [
+            self.first_son
+        ])
+
+    def test_has_no_sisters(self):
+        self.assertEqual(self.adam.get_sisters(), [])
+        self.assertEqual(self.first_daughter.get_sisters(), [])
+
+    def test_has_no_brothers(self):
+        self.assertEqual(self.adam.get_brothers(), [])
+
+    def test_has_no_self_in_sisters(self):
+        self.assertNotIn(self.first_daughter,
+                         self.first_daughter.get_sisters())
+
+    def test_has_no_self_in_brothers(self):
+        self.assertNotIn(self.first_son,
+                         self.first_son.get_brothers())
+
     def test_father_has_son(self):
         self.assertEqual(self.adam.children(gender='M'), [self.first_son])
 
@@ -34,6 +71,10 @@ class PersonTest(unittest.TestCase):
 
     def test_direct_successor(self):
         self.assertTrue(self.adam.is_direct_successor(self.first_son))
+        self.assertFalse(self.adam.is_direct_successor(self.adam))
+        self.assertTrue(self.eva.is_direct_successor(self.evas_secret_son))
+        self.assertFalse(self.adam.is_direct_successor(self.evas_secret_son))
+        self.assertFalse(self.adam.is_direct_successor(self.third_daughter))
 
 if __name__ == '__main__':
     unittest.main()
